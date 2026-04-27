f12fc1b1a8ef79556d3b037cb737468ef6e0628c
yirod3721 <ya.31.01.2008@gmail.com>
2026-04-27 23:59:04 +0300

    Added individual song lyric finding, Real folder choice and enhanced verbosity

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from lyric_catcher import lyric_catch
 from  metadata import metadata_ext
 import os
-song1 = 'Bruno Mars - Runaway Baby.mp3'
-folder1 = input("Enter the folder containing your MP3s: ").strip()
 def lyric_finder(song):
     artist_, title_, duration_1, album_ = metadata_ext(song)
+    print(f"the artist is {artist_} and the title is {title_} and the duration is {duration_1} and it belongs to {album_}")
     full_lyrics = lyric_catch(title_, artist_, album_, duration_1)
     if (full_lyrics == 0):
         print(f"Lyrics not found for {title_}")
@@ -36,18 +35,53 @@ def tester(folder):
                 print(f"{filename} already has an lrc file\n")
                 print("skipping")
             else:
-                print(f"Processing {filename}")
-                lrc_maker(filename)
+                mp3_path = os.path.join(folder, filename)
+                print(f"Processing {mp3_path}")
+                lrc_maker(mp3_path)
                 print(f"filename {filename} is done")
 
             
             
             
     print("Done")
+def individual_song(song):
+    song_path = os.path.join('.', song)
+    for filename in os.listdir('.'):
+        if os.path.exists(song_path):
+            print("MP3 file found, currently processing...")
+            lrc_maker(song_path)
+            print(f"{song} has been processed")
+            return 0
+        else:
+            print(f"file under name {song} in {song_path} not found")
+            return 1
+
+
+def main():
+    choice = str(input("enter song for individual song lyrics and folder for folder scan lyric function: "))
+    while (choice != "song") and (choice != "folder"):
+        print("Please enter either song or folder")
+        choice = str(input("enter song for individual song lyrics and folder for folder scan lyric function: "))
+    if (choice == "song"):
+        song1 = str(input("Enter the mp3 file name: "))
+        individual_song(song1)
+    if (choice == "folder"):
+        folder1 = input("Enter the folder containing your MP3s: ").strip()
+        tester(folder1)
+    print("Thank you for using my program")
 
 
 
-tester(folder1)
+
+main()
+
+
+
+
+
+        
+    
+
     
 
 
